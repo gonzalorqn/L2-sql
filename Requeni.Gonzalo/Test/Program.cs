05c773df18de972b990b4609d2922de45154ec92
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2019-06-12 09:01:01 -0300

    Add files via upload

---
+++ Requeni.Gonzalo/Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccesoDatos ad = new AccesoDatos();
+            List<Persona> lista = ad.TraerTodos();
+            foreach (Persona item in lista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+            Persona p = new Persona(2, "Rodrigo", "Perez", 62);
+            if (ad.AgregarPersona(p))
+                Console.WriteLine("Se agrego correctamente");
+            Console.ReadLine();
+        }
+    }
+}
